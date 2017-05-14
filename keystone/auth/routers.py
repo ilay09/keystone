31537d38c88cdb93a03fd5c87a0d219ca19b8dbe
ilusa usmanova <iusmanova@fix.ru>
2017-05-14 22:23:22 +0300

    change routers file

--- keystone/auth/routers.py
+++ keystone/auth/routers.py
@@ -11,6 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+# комментарий
 
 from keystone.auth import controllers
 from keystone.common import json_home
